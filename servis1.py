8280dbaf816748b901d289392ba7c41490d772c6
WiZARDatGH <112775356+WiZARDatGH@users.noreply.github.com>
2023-04-16 11:35:39 +0300

    Initial commit

---
+++ servis1.py
@@ -0,0 +1,59 @@
+import discord
+import asyncio
+from time import sleep
+from requests import get
+
+from sms import SendSms
+
+TOKEN = "MTA3OTg2NjMwMzM0MDM1NTY0NQ.GoGk8X.PlFXlTxSEIYDcxTh2KChfmWypkJBJYdzcc_mnk"
+gif = "https://images-ext-2.discordapp.net/external/lsrXcxRD8wtryrK3Zcge5SbuKar3hYEZEwThLVkpr6U/https/media.tenor.com/XOj-08_7du8AAAPo/john-bomb-rblxware.mp4"
+adet = 25
+saniye = 0
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('{} Çalışmaya Başladı!'.format(client.user))
+    activity = discord.Activity(type=discord.ActivityType.playing, name="Kartopu atmayı severim.")
+    await client.change_presence(activity=activity)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if len(message.content.split(" ")) == 2 and message.content.split(" ")[0] == "!sms":
+        if len(message.content.split(" ")[1]) == 10:
+            telno = message.content.split(" ")[1]
+            embed = discord.Embed(title="SMS Bomber (+90)",
+                                  description=(f"{adet} adet SMS Gönderiliyor --> {telno}\n{message.author.mention}"),
+                                  color=0x001eff, )
+            embed.set_thumbnail(url=gif)
+            await message.channel.send(embed=embed)
+            sms = SendSms(telno, "")
+            while sms.adet < adet:
+                for attribute in dir(SendSms):
+                    attribute_value = getattr(SendSms, attribute)
+                    if callable(attribute_value):
+                        if attribute.startswith('__') == False:
+                            if sms.adet == adet:
+                                break
+                            exec("sms." + attribute + "()")
+                            sleep(saniye)
+            await message.channel.send(
+                telno + " --> " + str(sms.adet) + f" adet SMS gönderildi. {message.author.mention}")
+        else:
+            await message.channel.send(
+                f"Geçerli komut yazınız!\nYardım için ' .help ' yazınız. {message.author.mention}")
+    elif "!help" == message.content:
+        await message.channel.send(
+            f"Sms göndermek için komutu aşağıdaki gibi yazınız.\n```!sms 5051234567 (+90 olmadan.)``` {message.author.mention}")
+    else:
+        pass
+
+
+client.run(TOKEN)
